c9962054ffa59d4b773d4fe3bc0b6a9d07dbdcf4
ironpup <christoph@prometheusresearch.com>
2017-01-16 13:35:10 -0500

    Connect html and index page.

--- main.js
+++ main.js
@@ -1,1 +1,5 @@
 console.log('Hello from Webpack');
+
+const name = 'Scotch.io';
+
+setTimeout(() => console.log((`Hello there from ${name}`), 300));
--- webpack.config.js
+++ webpack.config.js
@@ -1,19 +1,22 @@
 module.exports = {
-    entry: './main.js',
-    output: {
-        filename: './bundle.js'
-    },
-    watch: true,
-    module: {
-        loaders: [
-            {
-                test: /\.js$/,
-                exclude: /node_modules/,
-                loader: 'babel',
-                query: {
-                    presets: ['react', 'es2015']
-                }
-            }
-        ],
-    }
+  entry: './main.js',
+  output: {
+      filename: './bundle.js'
+  },
+  watch: true,
+  module: {
+      loaders: [
+          {
+              test: /\.js$/,
+              exclude: /node_modules/,
+              loader: 'babel',
+              query: {
+                  presets: ['react', 'es2015']
+              }
+          }
+      ],
+  },
+  resolve: {
+    extensions: ['', '.js', '.es6']
+  }
 };
